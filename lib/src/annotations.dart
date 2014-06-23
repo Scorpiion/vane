fb024b376f94d40dbff1103ff23e5b27f5efe811
Robert <Robert kerblom-Andersson>
2014-06-23 05:50:25 +0200

    Clean up and added some new http methods

--- lib/src/annotations.dart
+++ lib/src/annotations.dart
@@ -1,10 +1,20 @@
+// Copyright (c) 2014, Robert Åkerblom-Andersson <Robert@dartvoid.com>
+
 part of vane;
 
-const String GET    = "GET";
-const String POST   = "POST";
-const String PUT    = "PUT";
-const String DELETE = "DELETE";
+/// Http methods constants, declared for easy use with code completion and to
+/// avoid typos.
+const String GET      = "GET";
+const String HEAD     = "HEAD";
+const String POST     = "POST";
+const String PUT      = "PUT";
+const String DELETE   = "DELETE";
+const String TRACE    = "TRACE";
+const String OPTIONS  = "OPTIONS";
+const String CONNECT  = "CONNECT";
+const String PATCH    = "PATCH";
 
+/// [Route] annotation used to declared the request handler for a specific path
 class Route {
   final String path;
   final String method;
@@ -13,78 +23,14 @@ class Route {
   const Route(this.path, {this.method: "", this.methods: const []});
 }
 
+// A class is used for this constant since the other entries in the pipeline
+// list are also classes.
 class _This {
   const _This();
 }
 
+/// [This] is a constant used to indentify the main handler in middleware
+/// pipeline. With [This] it's possible to define where in the pipelien the
+/// main handler should go, per default that is last in the the pipeline.
 const This = const _This();
-
-String realname(DeclarationMirror mirror) {
-  return mirror.simpleName.toString().split('"')[1];
-}
-
-class _VaneRoute {
-  String name;
-  Route baseRoute;
-  List<_Handler> handlers = [];
-  List<ClassMirror> pre;
-  List<ClassMirror> post;
-  ClassMirror mirror;
-}
-
-class _Handler {
-  String name;
-  Route route;
-  UriParser parser;
-  List<String> parameters = [];
-}
-
-Route parseRoute(InstanceMirror routeMirror, [Route baseRoute]) {
-  Route route;
-
-  if(routeMirror.reflectee is Route) {
-    InstanceMirror pathMirror = routeMirror.getField(const Symbol("path"));
-    InstanceMirror methodMirror = routeMirror.getField(const Symbol("method"));
-    InstanceMirror methodMirrors = routeMirror.getField(const Symbol("methods"));
-//    print(" path     = ${path.reflectee}");
-//    print(" method   = ${method.reflectee}");
-//    print(" methods  = ${methods.reflectee}");
-
-    // Build
-    if(baseRoute == null) {
-      route = new Route(pathMirror.reflectee,
-                        method: methodMirror.reflectee,
-                        methods: methodMirrors.reflectee);
-    } else {
-      List<String> allMethods = new List();
-      List<String> methods;
-      String method;
-
-      // Let parameter method override baseRoutes method
-      if(methodMirror.reflectee == "") {
-        method = baseRoute.method;
-      } else {
-        method = methodMirror.reflectee;
-      }
-
-      // Avoid adding duplicate methods
-      allMethods.addAll(baseRoute.methods);
-      allMethods.addAll(methodMirrors.reflectee);
-
-      for(var method in allMethods) {
-        if(methods.contains(method) == false) {
-          methods.add(method);
-        }
-      }
-
-      route = new Route("${baseRoute.path}${pathMirror.reflectee}",
-                        method: method,
-                        methods: methods);
-    }
-  } else {
-    print("${routeMirror.reflectee} is not Route");
-  }
-
-  return route;
-}
 
