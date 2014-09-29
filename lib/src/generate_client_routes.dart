c871dd50facc0e4174b6db93e677dbb863d92d29
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-09-29 15:24:47 +0200

    Added support for SPA paths

--- lib/src/generate_client_routes.dart
+++ lib/src/generate_client_routes.dart
@@ -35,31 +35,29 @@ List<_VaneRoute> generateClientRoutes() {
   // TODO: Consider the value of "match" as well to reduce risk of differences
   //       between nginx and pub serve serving files. Now we are forcing
   //       the match to be "regex" for all dir handlers.
-  for(var handler in appConf["client"]) {
-    var url = handler["url"];
-    var httpMethod = handler["method"];
-    if(httpMethod == null) {
-      httpMethod = "";
-    }
+  if(appConf["client"] != null) {
+    for(var handler in appConf["client"]) {
+      var url = handler["url"];
+      var httpMethod = handler["method"];
+      if(httpMethod == null) {
+        httpMethod = "";
+      }
 
-    if(url != null) {
-      // Setup podo controllers
-      var mirror = VaneClientProxyMirror.type;
-      String controller;
-      Route baseMetaRoute;
+      if(url != null) {
+        // Setup podo controllers
+        var mirror = VaneClientProxyMirror.type;
+        String controller;
+        Route baseMetaRoute;
 
-      Logger.root.fine("Adding routes for podo controller: ${realname(mirror)}");
+        Logger.root.fine("Adding routes for podo controller: ${realname(mirror)}");
 
-      // Add name and mirror to route
-      controller = realname(mirror);
+        // Add name and mirror to route
+        controller = realname(mirror);
 
-      // Create a new route object for each handler method (better for runtime
-      // performance to give each handler their own route object since it makes
-      // matching easier)
-      for(var method in mirror.declarations.values.where((method)
-          => method is MethodMirror && method.isRegularMethod)) {
-
+        // Create a new route object
+        MethodMirror method = mirror.declarations[#pubProxy];
         _VaneRoute route = new _VaneRoute();
+
         route.controller = controller;
         route.type = _podo;
         route.classMirror = mirror;
@@ -76,6 +74,58 @@ List<_VaneRoute> generateClientRoutes() {
         Logger.root.fine(" Adding podo handler \"${realname(mirror)}.${realname(method)}\" with path ${url}");
         routes.add(route);
       }
+    }
+  }
+
+  // For each spa handler, add a new vane function handler route
+  if(appConf["spa"] != null) {
+    for(var handler in appConf["spa"]) {
+      var url = handler["url"];
+      var httpMethod = handler["method"];
+      if(httpMethod == null) {
+        httpMethod = "";
+      }
+
+      // Add path to spa paths
+      VaneClientProxy.spaPaths.add(url);
+
+      if(url != null) {
+        // Setup podo controllers
+        var mirror = VaneClientProxyMirror.type;
+        String controller;
+        Route baseMetaRoute;
+
+        Logger.root.fine("Adding routes for podo controller: ${realname(mirror)}");
+
+        // Add name and mirror to route
+        controller = realname(mirror);
+
+        // Create a new route object
+        MethodMirror method = mirror.declarations[#spaProxy];
+        _VaneRoute route = new _VaneRoute();
+
+        route.controller = controller;
+        route.type = _podo;
+        route.classMirror = mirror;
+
+        route.method = realname(method);
+        route.metaRoute = new Route(url, method: httpMethod, methods: []);
+        route.parser = new UriParser(new UriTemplate(url));
+        for(VariableMirror parameter in method.parameters) {
+          Logger.root.fine("  Parameter = ${realname(parameter)}");
+          route.parameters.add(realname(parameter));
+        }
+
+        // Save route to controller
+        Logger.root.fine(" Adding podo handler \"${realname(mirror)}.${realname(method)}\" with path ${url}");
+        routes.add(route);
+      }
+
+      // Sort SPA paths
+      VaneClientProxy.spaPaths.sort((x, y) => x.length.compareTo(y.length));
+
+      // Reverse list so that the longest paths comes first
+      VaneClientProxy.spaPaths = new List.from(VaneClientProxy.spaPaths.reversed);
     }
   }
 
