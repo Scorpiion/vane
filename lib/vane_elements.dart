4c4443c22913884ce3b5ed32f6533a0fac30bb69
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2015-04-06 21:08:20 +0200

    First step in restructuring of Vane into seperate packages

--- lib/vane_elements.dart
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2014, Robert Åkerblom-Andersson <Robert@dartvoid.com>
-
-library vane_elements;
-
-export 'package:vane/elements/vane_router/vane_router.dart';
-export 'package:vane/elements/vane_view/vane_view.dart';
-
