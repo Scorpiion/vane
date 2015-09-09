3f6ba8dde8663a06566651641943b4c32dad688e
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2015-09-09 14:39:08 +0200

    Added missing setters

--- vane_server/lib/src/res.dart
+++ vane_server/lib/src/res.dart
@@ -27,6 +27,9 @@ class VaneResponse {
    * -1 - which is also the default if not set.
    */
   int get contentLength => zResponse.contentLength;
+  void set contentLength(int _contentLength) {
+    zResponse.contentLength = _contentLength;
+  }
 
   /**
    * Gets and sets the persistent connection state. The initial value
@@ -34,12 +37,18 @@ class VaneResponse {
    * request.
    */
   bool get persistentConnection => zResponse.persistentConnection;
+  void set persistentConnection(bool _persistentConnection) {
+    zResponse.persistentConnection = _persistentConnection;
+  }
 
   /**
    * Gets and sets the reason phrase. If no reason phrase is explicitly
    * set a default reason phrase is provided.
    */
   String get reasonPhrase => zResponse.reasonPhrase;
+  void set reasonPhrase(String _reasonPhrase) {
+    zResponse.reasonPhrase = _reasonPhrase;
+  }
 
   /**
    * Gets and sets the status code. Any integer value is accepted. For
@@ -48,9 +57,8 @@ class VaneResponse {
    * value [HttpStatus.OK] is used.
    */
   int get statusCode => zResponse.statusCode;
-
-  void set statusCode(int code) {
-    zResponse.statusCode = code;
+  void set statusCode(int _statusCode) {
+    zResponse.statusCode = _statusCode;
   }
 
   /**
@@ -70,6 +78,9 @@ class VaneResponse {
    * The [deadline] is `null` by default.
    */
   Duration get deadline => zResponse.deadline;
+  void set deadline(Duration _deadline) {
+    zResponse.deadline = _deadline;
+  }
 
   /**
    * Gets information about the client connection. Returns [:null:] if the
