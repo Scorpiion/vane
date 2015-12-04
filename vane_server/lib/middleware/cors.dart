1f6d66998ae24f02779d7676fcf472dcc4e33776
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2015-12-04 14:41:05 +0100

    Save

--- vane_server/lib/middleware/cors.dart
+++ vane_server/lib/middleware/cors.dart
@@ -5,7 +5,8 @@ class Cors extends Vane {
   Future main() {
     // Allow cross origin requests
     res.headers.add("Access-Control-Allow-Origin", "*");
-    res.headers.add("Access-Control-Allow-Headers", "Content-Type, content-type");
+    // TODO: Remove DartVoid specific headers from here...
+    res.headers.add("Access-Control-Allow-Headers", "content-type, dv-api-client, dv-session-token, dv-user-agent");
     res.headers.add("Access-Control-Allow-Methods", "OPTIONS, HEAD, GET, POST, PUT, DELETE");
 
     if(req.method == "OPTIONS") {
