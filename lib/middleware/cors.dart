85e331ff3b4daa316706ae322762024aafa34815
Robert Åkerblom-Andersson <robert@sourcevoid.com>
2017-02-11 16:04:33 +0100

    Changed old cors header values

--- lib/middleware/cors.dart
+++ lib/middleware/cors.dart
@@ -6,7 +6,7 @@ class Cors extends Vane {
     // Allow cross origin requests
     res.headers.add("Access-Control-Allow-Origin", "*");
     // TODO: Remove DartVoid specific headers from here...
-    res.headers.add("Access-Control-Allow-Headers", "content-type, dv-api-client, dv-session-token, dv-user-agent");
+    res.headers.add("Access-Control-Allow-Headers", "content-type, sv-api-client, sv-session-token, sv-user-agent");
     res.headers.add("Access-Control-Allow-Methods", "OPTIONS, HEAD, GET, POST, PUT, DELETE");
 
     if(req.method == "OPTIONS") {
